7f3494c8531b03962fc819881c36853f926b80e6
Firebird Installer <firebird@nixfred.com>
2025-08-03 03:13:56 -0400

    Fix GitHub token authentication format

--- install.sh
+++ install.sh
@@ -57,7 +57,8 @@ fi
 
 # Try to clone with token
 echo -e "${YELLOW}Cloning firebird...${NC}"
-if git clone "https://${GITHUB_TOKEN}@github.com/nixfred/firebird.git" "$HOME/firebird" 2>/dev/null; then
+# Use token as username with 'x-oauth-basic' as password
+if git clone "https://${GITHUB_TOKEN}:x-oauth-basic@github.com/nixfred/firebird.git" "$HOME/firebird" 2>&1 | grep -v "Cloning into"; then
     echo -e "${GREEN}✓ Cloned successfully!${NC}"
     
     # Remove token from git config for security
